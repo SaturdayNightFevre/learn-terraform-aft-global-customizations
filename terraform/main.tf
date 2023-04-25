f2c239e2d22a3e191ece90775c6303a8cd5e48f1
John Fevre <john.fevre@stagecoach.com>
2023-04-25 15:11:54 +0100

    test: remove storage & config

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,29 +14,3 @@ module "config_conformance_pack" {
   #global_resource_collector_region = local.region.locals.aws_region
   #tags = merge(local.common_tags.locals.default_tags, local.env_tags)
 }
-
-module "config" {
-  source  = "cloudposse/config/aws"
-  version = "0.17.0"
-  create_sns_topic = true
-  create_iam_role  = true
-  #  managed_rules                    = dependency.conformance_pack.outputs.rules.
-  force_destroy                    = true
-  s3_bucket_id                     = module.config_storage.bucket_id
-  s3_bucket_arn                    = module.config_storage.bucket_arn
-  global_resource_collector_region = "eu-west-2"
-  central_resource_collector_account = "605188504569"
-  #tags                             = merge(local.common_tags.locals.default_tags, local.env_tags)
-}
-
-
-module "config_storage" {
-  source  = "cloudposse/config-storage/aws"
-  version = "0.8.1"
-
-  name                     = "${data.aws_caller_identity.current.account_id}-aws-config-testing"
-  force_destroy            = true
-  enabled                  = true
-  standard_transition_days = 30
-  #tags                     = merge(local.common_tags.locals.default_tags, local.env_tags)
-} 
